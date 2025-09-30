98bf8092b3ff9c502c1bf2c205a36821d855b3c5
johnnybravo <ezeufohchibuzor@gmail.com>
2025-09-30 18:45:08 +0100

    sidebar components created and other minor components

--- lib/appwrite/config.ts
+++ lib/appwrite/config.ts
@@ -5,5 +5,5 @@ export const appwriteConfig = {
   usersTableId: process.env.NEXT_PUBLIC_APPWRITE_USERS_TABLE_NAME!,
   filesTableId: process.env.NEXT_PUBLIC_APPWRITE_FILES_TABLE_NAME!,
   bucketId: process.env.NEXT_PUBLIC_APPWRITE_BUCKET!,
-  secretKey: process.env.NEXT_APPWRITE_KEY!,
+  secretKey: process.env.NEXT_APPWRITE_SECRET!,
 };
